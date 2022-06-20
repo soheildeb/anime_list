f10fd87e431135ca5cee983fafde824b7912b6ef
soheil <soheil.u.u.1384@gmail.com>
2022-06-20 10:34:19 +0430

    create list page (index.html, urls, index views and Anime model created)

--- animeworld/list/views.py
+++ animeworld/list/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
+from .models import Anime
+from django.http import HttpResponse
 
+def index(request):
+        template = loader.get_template('index.html')
+        anime_list = Anime.objects.all().values()
+        context = {
+                'list':anime_list,
+        }
+
+        return HttpResponse(template.render(context,request))
